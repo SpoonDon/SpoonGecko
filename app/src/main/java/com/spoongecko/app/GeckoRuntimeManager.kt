12ee060325b8b6c402fde39f912d3a5e2ea85a81
Spoon Don <291380160+SpoonDon@users.noreply.github.com>
2026-08-08 15:45:40 +0530

    Update GeckoRuntimeManager.kt

--- app/src/main/java/com/spoongecko/app/GeckoRuntimeManager.kt
+++ app/src/main/java/com/spoongecko/app/GeckoRuntimeManager.kt
@@ -11,23 +11,18 @@ object GeckoRuntimeManager {
     fun getRuntime(context: Context): GeckoRuntime {
         if (runtime == null) {
             val cbSettings = ContentBlocking.Settings.Builder()
-                .antiTracking(ContentBlocking.AntiTracking.STRICT)
+                .antiTracking(ContentBlocking.AntiTracking.DEFAULT)
                 .safeBrowsing(ContentBlocking.SafeBrowsing.DEFAULT)
                 .cookieBehavior(ContentBlocking.CookieBehavior.ACCEPT_NON_TRACKERS)
                 .build()
 
             val runtimeSettings = GeckoRuntimeSettings.Builder()
-    .contentBlocking(cbSettings)
-    .extensionsProcessEnabled(true)
-    .useMultiprocess(true)
-    .preferredColorScheme(GeckoRuntimeSettings.COLOR_SCHEME_SYSTEM)
-    .build()
+                .contentBlocking(cbSettings)
+                .extensionsProcessEnabled(true)
+                .preferredColorScheme(GeckoRuntimeSettings.COLOR_SCHEME_SYSTEM)
+                .build()
+
             runtime = GeckoRuntime.create(context.applicationContext, runtimeSettings)
-            runtime?.delegate = object : GeckoRuntime.Delegate {
-    override fun onShutdown() {
-        runtime = null
-    }
-}
         }
         return runtime!!
     }
