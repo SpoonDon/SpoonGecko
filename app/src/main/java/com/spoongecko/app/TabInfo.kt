5cbb6e2a1619300cb983a11a94c29f1a2026a444
Spoon Don <291380160+SpoonDon@users.noreply.github.com>
2026-08-09 18:33:09 +0530

    Update TabInfo.kt

--- app/src/main/java/com/spoongecko/app/TabInfo.kt
+++ app/src/main/java/com/spoongecko/app/TabInfo.kt
@@ -1,10 +1,13 @@
 package com.spoongecko.app
+
 import org.mozilla.geckoview.GeckoSession
+import org.mozilla.geckoview.GeckoSession.SessionState
 
 data class TabInfo(
     val session: GeckoSession,
     var title: String = "New Tab",
     var url: String = "",
     var canGoBack: Boolean = false,
-    var canGoForward: Boolean = false
+    var canGoForward: Boolean = false,
+    var sessionState: SessionState? = null
 )
