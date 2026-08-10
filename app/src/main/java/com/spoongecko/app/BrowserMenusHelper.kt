2fff679e7b7904c053d19bcc64bde214cff91b60
Spoon Don <291380160+SpoonDon@users.noreply.github.com>
2026-08-10 09:08:46 +0530

    Update BrowserMenusHelper.kt

--- app/src/main/java/com/spoongecko/app/BrowserMenusHelper.kt
+++ app/src/main/java/com/spoongecko/app/BrowserMenusHelper.kt
@@ -165,14 +165,16 @@ class BrowserMenusHelper(
             )
         }
         load()
-
+        
         search?.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                val query = s?.toString() ?: ""
-                load(query)
+            
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val text = s?.toString() ?: ""
+                load(text)
             }
+            
+            override fun afterTextChanged(s: Editable?) {}
         })
         
         view.findViewById<ImageButton>(R.id.btn_delete_all_history)?.setOnClickListener {
